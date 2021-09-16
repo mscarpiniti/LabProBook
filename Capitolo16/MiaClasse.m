31d86fc4d5ad5d39aac47b4c0fd122fb02e69226
Michele Scarpiniti <michele.scarpiniti@uniroma1.it>
2021-09-16 19:47:46 +0200

    Add files via upload

---
+++ Capitolo16/MiaClasse.m
@@ -0,0 +1,37 @@
+classdef MiaClasse
+% Una classe con due proprietà e due metodi che calcolano
+% la somma e il prodotto delle due proprietà.
+% Utilizzo:
+%   m = MiaClasse;
+%
+% Vedi anche: ....
+
+   properties(Access = private)
+      a = 3;  % Primo parametro
+	  b = 2;  % Secondo parametro
+   end
+   
+   properties(Constant)
+       c = 3.14;
+   end
+
+   methods
+      function s = Somma(obj)
+      % Calcola la somma delle due proprietà
+         s = obj.a + obj.b;  % Semplice esempio
+      end
+
+      function p = Prodotto(obj)
+      % Calcola il prodotto delle due proprietà
+         p = obj.a * obj.b;
+      end
+      
+   end
+   
+   methods(Static)
+      function s = descrizione()
+         s = 'Classe per gestire il mio progetto...';
+      end
+   end
+
+end
