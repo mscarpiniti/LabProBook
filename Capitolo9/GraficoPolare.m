b06a5454a1367e9e58132f09ef6d5cb146cd1f2b
Michele Scarpiniti <michele.scarpiniti@uniroma1.it>
2022-12-11 20:35:09 +0100

    Delete Capitolo9 directory

--- Capitolo9/GraficoPolare.m
+++
@@ -1,8 +0,0 @@
-% Esempio di grafico polare
-%
-% M. Scarpiniti (Dip. DIET - Sapienza Università di Roma)
-
-x = 0:pi/100:pi;
-y = sin(x);
-polarplot(x, y);
-title('Funzione sinusoidale in coordinate polari');
